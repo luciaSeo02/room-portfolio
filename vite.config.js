46ab1251e4ea8fd7ba07769a6c164fecef8272a3
Lucía Seoane <luciaseo20@gmail.com>
2026-03-04 18:59:51 +0100

    update deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/room-portfolio/",
+  base: "/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 });
